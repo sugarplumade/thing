ad9dc80e68645d4f84b6163ae0dc59f0e061623b
sugarplumade <132719110+sugarplumade@users.noreply.github.com>
2024-03-10 10:22:03 -0700

    Initial Commit

---
+++ Thing/ThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingApp.swift
+//  Thing
+//
+//  Created by Adekunle Somade on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
